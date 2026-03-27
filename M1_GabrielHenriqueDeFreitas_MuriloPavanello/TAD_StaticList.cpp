93287f607c74a857fde879836feb61a0c1500172
ghenriqf <gabrielhenriquefreitas28@gmail.com>
2026-03-27 15:24:45 -0300

    implementa estrutura inicial da lista estatica

--- M1_GabrielHenriqueDeFreitas_MuriloPavanello/TAD_StaticList.cpp
+++ M1_GabrielHenriqueDeFreitas_MuriloPavanello/TAD_StaticList.cpp
@@ -29,8 +29,57 @@ StaticList *initialize_list(int capacity)
     return static_list;
 }
 
+bool insert_sensor(Sensor sensor, StaticList *static_list)
+{
+    if (static_list->elements_count >= static_list->capacity)
+        return false;
+
+    *(static_list->sensors + static_list->elements_count) = sensor;
+    static_list->elements_count++;
+
+    return true;
+}
+
+Sensor *remove_sensor(int value, StaticList *static_list)
+{
+    for (int i = 0; i <= static_list->elements_count; i++)
+    {
+        Sensor *sensor = static_list->sensors;
+
+        if (sensor->value == value)
+            return sensor;
+
+        sensor++;
+    }
+
+    return nullptr;
+}
+
+void print_sensor(StaticList *static_list)
+{
+    for (int i = 0; i <= static_list->capacity; i++)
+    {
+        cout << static_list->sensors->value << endl;
+    }
+}
+
 int main()
 {
+
+    StaticList *static_list = initialize_list(5);
+
+    Sensor sensor;
+    sensor.value = 10;
+
+    insert_sensor(sensor, static_list);
+    insert_sensor(sensor, static_list);
+    insert_sensor(sensor, static_list);
+    insert_sensor(sensor, static_list);
+    insert_sensor(sensor, static_list);
+    insert_sensor(sensor, static_list);
+    insert_sensor(sensor, static_list);
+
+    print_sensor(static_list);
 
     return 0;
 }
--- M1_GabrielHenriqueDeFreitas_MuriloPavanello/TAD_StaticList.h
+++ M1_GabrielHenriqueDeFreitas_MuriloPavanello/TAD_StaticList.h
@@ -13,8 +13,8 @@ typedef struct StaticList
     int elements_count;
 };
 
-void insert_sensor(Sensor sensor, StaticList *static_list);
-void remove_sensor(int value, StaticList *static_list);
+bool insert_sensor(Sensor sensor, StaticList *static_list);
+bool remove_sensor(int value, StaticList *static_list);
 StaticList *initialize_list(int capacity);
 
 #endif
